cce18d3102fd148c410be405a17ff507ed242dbd
sourabhnoob <151834993+sourabhnoob@users.noreply.github.com>
2024-02-10 01:12:26 +0530

    9 create a middleware and a function to handle possible errors

--- api/index.js
+++ api/index.js
@@ -27,3 +27,13 @@ app.get('/test',(req,res)=>{
 
 app.use('/api/user',userRouter)
 app.use('/api/auth',authRouter)
+
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal server error';
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message,
+    })
+})
